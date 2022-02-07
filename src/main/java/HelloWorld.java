0b622a7c0e72f264209e975e26b1c7311fce5097
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-07 21:06:01 +0100

    Prvi Commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang.StringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World Bitches!");
+        System.out.println(StringUtils.capitalize("hello world!"));
+    }
+}
